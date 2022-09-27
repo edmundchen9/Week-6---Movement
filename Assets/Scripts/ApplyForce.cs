9d493a9f10d7fc6b57e0bab6e3dec87648490ce5
Edmund Chen <edchen7321@gmail.com>
2022-09-27 11:40:45 -0400

    bunch of updates

--- Assets/Scripts/ApplyForce.cs
+++ Assets/Scripts/ApplyForce.cs
@@ -2,26 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveObject02 : MonoBehaviour
+public class ApplyForce : MonoBehaviour
 {
     Rigidbody rigidbody;
-    [SerializeField] Vector3 moveDirection;
+    [SerializeField] Vector3 forceDirection;
+
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
         ApplyInstantForce();
     }
-    
- void ApplyInstantForce()
+
+    void ApplyInstantForce()
     {
-    GetComponent<Rigidbody>().AddForce(moveDirection, ForceMode.Impulse);
+    rigidbody.AddForce(forceDirection, ForceMode.Impulse);
     }
 
     // Update is called once per frame
     void Update()
     {
-     GetComponent<Rigidbody>().AddForce(moveDirection, ForceMode.Acceleration);
+     rigidbody.AddForce(forceDirection, ForceMode.Acceleration);
     }
-
 }
